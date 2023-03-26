6df4c253acbb0b9a84a438f3561b63dbb5c7b7f9
henrilaats <72796703+henrilaats@users.noreply.github.com>
2023-03-26 18:21:18 +0300

    Update TaskTests.java

--- src/test/java/devqa_task_2023/TaskTests.java
+++ src/test/java/devqa_task_2023/TaskTests.java
@@ -3,18 +3,15 @@ package devqa_task_2023;
 import org.junit.Test;
 
 public class TaskTests extends TestHelper {
-
-    private static final String POSITION_NAME = "Development QA Engineer (Intern)";
-
     @Test
     public void verifyPositionShownOnInternshipPage() {
         navigateToInternshipPage();
-        verifyIfPositionIsShown(POSITION_NAME);
+        verifyIfPositionIsShown();
     }
 
     @Test
     public void verifyPositionShownOnPageUsingCoordinates() {
         navigateToPageByCoordinatesClick();
-        verifyIfPositionIsShown(POSITION_NAME);
+        verifyIfPositionIsShown();
     }
 }
